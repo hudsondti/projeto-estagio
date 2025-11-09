c8c1ece4b3f08aa122f45d4925b4ee72f388c505
hudsondti <hudson.junior@dtidigital.com.br>
2025-11-09 14:39:36 -0300

    subindo mudanças

--- app/meus-estagios/upadateModal.tsx
+++ app/meus-estagios/upadateModal.tsx
@@ -13,7 +13,7 @@ export default function UpdateModal() {
         </h1>
         <Link
           href="/internshiplist"
-          className="px-4 py-2 bg-blue-500 text-white rounded-md"
+          className="px-4 py-2 bg-blue-500 text-white rounded-md "
         >
           Inicio
         </Link>
